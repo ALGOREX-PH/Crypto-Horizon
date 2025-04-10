96d63f4ee2bcec0e85c94ee8dd99dc95dffd0f4d
unknown <debbiemeer30@gmail.com>
2025-04-10 18:35:10 +0800

    updated next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export'
+  output: 'export',
+  webpack: (config, { dev, isServer }) => {
+    // Disable webpack caching in development to prevent file system errors
+    if (dev) {
+      config.cache = false;
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
